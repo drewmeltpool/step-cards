f3af49468b90cddc7f52fed729031a3906e87294
drewmeltpool <drewmelt132@gmail.com>
2021-04-21 23:52:57 +0300

    fix err

--- js/pages/homepage.js
+++ js/pages/homepage.js
@@ -5,41 +5,41 @@ import { ControlPage } from './controlPage.js'
 const root = document.querySelector('#root')
 
 export const HomePage = new ElementBuild()
-    .tag('nav')
-    .parent(root)
-    .options({ className: 'nav' })
-    .children(
-        new ElementBuild()
-            .tag('div')
-            .options({ className: 'navinner-wrapper container' })
-            .children(
-                new ElementBuild()
-                    .tag('h1')
-                    .options({ className: 'logo', textContent: 'Medico' }),
-                new ElementBuild()
-                    .tag('ul')
-                    .options({ className: 'navlist' })
-                    .children(
-                        new ElementBuild()
-                            .tag('li')
-                            .options({ className: 'navitem' })
-                            .children(
-                                new ElementBuild()
-                                    .tag('button')
-                                    .options({
-                                        className: 'btn btn--default',
-                                        textContent: 'Log in',
-                                    })
-                                    .eventListener('click', () => {
-                                        new Redirect(ControlPage).redirect()
-                                    })
-                            ),
-                        new ElementBuild()
-                            .tag('li')
-                            .options({ className: 'navitem', textContent: 'test2' }),
-                        new ElementBuild()
-                            .tag('li')
-                            .options({ className: 'nav__item', textContent: 'test3' })
-                    )
-            )
-    )
+	.tag('nav')
+	.parent(root)
+	.options({ className: 'nav' })
+	.children(
+		new ElementBuild()
+			.tag('div')
+			.options({ className: 'nav__inner-wrapper container' })
+			.children(
+				new ElementBuild()
+					.tag('h1')
+					.options({ className: 'logo', textContent: 'Medico' }),
+				new ElementBuild()
+					.tag('ul')
+					.options({ className: 'nav__list' })
+					.children(
+						new ElementBuild()
+							.tag('li')
+							.options({ className: 'nav__item' })
+							.children(
+								new ElementBuild()
+									.tag('button')
+									.options({
+										className: 'btn btn--default',
+										textContent: 'Log in',
+									})
+									.eventListener('click', () => {
+										new Redirect(ControlPage).redirect()
+									})
+							),
+						new ElementBuild()
+							.tag('li')
+							.options({ className: 'nav__item', textContent: 'test2' }),
+						new ElementBuild()
+							.tag('li')
+							.options({ className: 'nav__item', textContent: 'test3' })
+					)
+			)
+	)
