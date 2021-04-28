55323ce92112549a1b2e76b810d3c25b75d5a08c
drewmeltpool <drewmelt132@gmail.com>
2021-04-28 17:30:37 +0300

    Revert "bad changes"
    This reverts commit d7b4a86feec344f09bd8a0df43d4a9fc0337ca6e.

--- js/components/Doctor/VisitForm.js
+++ js/components/Doctor/VisitForm.js
@@ -1,8 +1,10 @@
 import { getInputValue } from '../DOM/dom.js'
 import { Form } from '../layouts/Form.js'
+import { Api } from '../../api/api.js'
 import { Loader } from '../../components/layouts/Loader.js'
 import { PatientItem } from '../../components/Patient/PatientItem.js'
-import { DentistForm } from '../Form/DentistForm.js'
+import { Redirect } from '../../redirect/redirect.js'
+import { ControlPage } from '../../pages/control/Cards.js'
 import { VisitDentist } from './Visit.js'
 
 export class VisitForm {
@@ -14,7 +16,53 @@ export class VisitForm {
 		const type = this.type
 		switch (type) {
 			case 'dentist': {
-				return new DentistForm()
+				return new Form('Стоматолог')
+					.input({ id: 'goal', type: 'text', placeholder: 'Цель визита' })
+					.textArea({ id: 'desсription' })
+					.select(
+						{ id: 'priority' },
+						{ textContent: 'Срочность', disabled: true },
+						{ value: 'low', textContent: 'обычная' },
+						{ value: 'medium', textContent: 'приоритетная' },
+						{ value: 'high', textContent: 'неотложная' },
+					)
+					.input({ id: 'fullname', type: 'text', placeholder: 'ФИО' })
+					.input({
+						id: 'date',
+						type: 'date',
+						placeholder: 'Дата последнего визита',
+					})
+					.button({ textContent: 'Создать карточку' })
+					.submit(async () => {
+						const loader = new Loader()
+						loader.render()
+						const api = new Api()
+						api.setToken(localStorage.getItem('token'))
+						const data = {
+							doctor: 'Стоматлог',
+							specialization: 'dentist',
+							goal: getInputValue('#goal'),
+							description: getInputValue('#desсription'),
+							priority: getInputValue('#priority'),
+							patient: getInputValue('#fullname'),
+							date: getInputValue('#date'),
+						}
+						const list = document.querySelector('.patient__list')
+						const card = await api.addCard(data)
+						const cards = localStorage.getItem('cards')
+						const cardsParse = JSON.parse(cards)
+						const ans = JSON.stringify([...JSON.parse(cards), card])
+						localStorage.setItem('cards', ans)
+						if (!cardsParse.length) {
+							new Redirect(ControlPage()).redirect()
+							return
+						}
+						new PatientItem(card).parent(list).render()
+						document.querySelector('.modal-wrapper').remove()
+						document.body.classList = ''
+						loader.remove()
+					})
+					.build()
 			}
 			case 'therapist': {
 				return new Form('Терапевт')
@@ -35,6 +83,8 @@ export class VisitForm {
 					.submit(async () => {
 						const loader = new Loader()
 						loader.render()
+						const api = new Api()
+						api.setToken(localStorage.getItem('token'))
 						const data = {
 							doctor: 'Терапевт',
 							specialization: 'therapist',
@@ -44,7 +94,17 @@ export class VisitForm {
 							patient: getInputValue('#fullname'),
 							age: getInputValue('#age'),
 						}
-						await new PatientItem().add(data)
+						const list = document.querySelector('.patient__list')
+						const card = await api.addCard(data)
+						const cards = localStorage.getItem('cards')
+						const cardsParse = JSON.parse(cards)
+						const ans = JSON.stringify([...JSON.parse(cards), card])
+						localStorage.setItem('cards', ans)
+						if (!cardsParse.length) {
+							new Redirect(ControlPage()).redirect()
+							return
+						}
+						new PatientItem(card).parent(list).render()
 						document.querySelector('.modal-wrapper').remove()
 						document.body.classList = ''
 						loader.remove()
@@ -85,6 +145,8 @@ export class VisitForm {
 					.submit(async () => {
 						const loader = new Loader()
 						loader.render()
+						const api = new Api()
+						api.setToken(localStorage.getItem('token'))
 						const data = {
 							doctor: 'Кардиолог',
 							specialization: 'cardiologist',
@@ -96,8 +158,18 @@ export class VisitForm {
 							pressure: getInputValue('#pressure'),
 							weight: getInputValue('#weightindex'),
 						}
-
-						await new PatientItem().add(data)
+						console.log(data)
+						const list = document.querySelector('.patient__list')
+						const card = await api.addCard(data)
+						const cards = localStorage.getItem('cards')
+						const cardsParse = JSON.parse(cards)
+						const ans = JSON.stringify([...JSON.parse(cards), card])
+						localStorage.setItem('cards', ans)
+						if (!cardsParse.length) {
+							new Redirect(ControlPage()).redirect()
+							return
+						}
+						new PatientItem(card).parent(list).render()
 						document.querySelector('.modal-wrapper').remove()
 						document.body.classList = ''
 						loader.remove()
--- js/components/Patient/PatientList.js
+++ js/components/Patient/PatientList.js
@@ -3,7 +3,7 @@ import { PatientItem } from './PatientItem.js'
 
 export class PatientList {
 	constructor(data) {
-		const patients = data.map(patient => new PatientItem().create(patient))
+		const patients = data.map(patient => new PatientItem(patient))
 		return new Element()
 			.tag('div')
 			.options({ className: 'patient__list' })
