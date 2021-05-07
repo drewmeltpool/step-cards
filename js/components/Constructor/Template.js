7550aec47399b7dec136502bdfa398472b43e5cb
drewmeltpool <drewmelt132@gmail.com>
2021-05-07 23:24:02 +0300

    look

--- js/components/Constructor/Template.js
+++ js/components/Constructor/Template.js
@@ -1,75 +1,74 @@
 import { Element } from './Element.js'
 
 export class Button {
-	constructor(className = '', textContent = '') {
-		return new Element()
-			.tag('button')
-			.options({ className: `btn ${className}`, textContent })
-	}
+    constructor(className = '', textContent = '') {
+        return new Element()
+            .tag('button')
+            .options({ className: `btn ${className}`, textContent })
+    }
 }
 
 export class Icon {
-	constructor(className) {
-		return new Element().tag('i').options({ className })
-	}
+    constructor(className) {
+        return new Element().tag('i').options({ className })
+    }
 }
 
 export class Logo {
-	constructor() {
-		return new Element()
-			.tag('div')
-			.options({ className: 'logo-wrapper' })
-			.children(
-				new Element()
-					.tag('h1')
-					.options({ className: 'logo', textContent: 'Medico' }),
-				new Icon('fas fa-heartbeat'),
-			)
-	}
+    constructor() {
+        return new Element()
+            .tag('div')
+            .options({ className: 'logo-wrapper' })
+            .children(
+                new Element()
+                .tag('h1')
+                .options({ className: 'logo', textContent: 'Medico' }),
+                new Icon('fas fa-heartbeat'),
+            )
+    }
 }
 
 export class Nav {
-	constructor(...elems) {
-		return new Element()
-			.tag('nav')
-			.options({ className: 'nav' })
-			.children(
-				new Element()
-					.tag('div')
-					.options({ className: 'nav__inner-wrapper container' })
-					.children(...elems),
-			)
-	}
+    constructor(...elems) {
+        return new Element()
+            .tag('nav')
+            .options({ className: 'nav' })
+            .children(
+                new Element()
+                .tag('div')
+                .options({ className: 'nav__inner-wrapper container' })
+                .children(...elems),
+            )
+    }
 }
 
 export class List {
-	constructor(listClass, childClass) {
-		this.listClass = listClass
-		this.childClass = childClass
-		this.list = new Element().tag('ul').options({ className: this.listClass })
-	}
+    constructor(listClass, childClass) {
+        this.listClass = listClass
+        this.childClass = childClass
+        this.list = new Element().tag('ul').options({ className: this.listClass })
+    }
 
-	elements(...items) {
-		items.forEach(item => {
-			const listItem = new Element().tag('li').options({
-				className: this.childClass,
-			})
-			listItem.children(item)
-			this.list.children(listItem)
-		})
+    elements(...items) {
+        items.forEach(item => {
+            const listItem = new Element().tag('li').options({
+                className: this.childClass,
+            })
+            listItem.children(item)
+            this.list.children(listItem)
+        })
+        return this.list
+    }
 
-		return this.list
-	}
-
-	build() {
-		return this.list
-	}
+    build() {
+        return this.list
+    }
 }
 
 export class Header {
-	constructor(...items) {
-		this.header = new Element().tag('header').options({ className: 'hero' })
-		items.forEach(item => this.header.children(item))
-		return this.header
-	}
+    constructor(...items) {
+        this.header = new Element().tag('header').options({ className: 'hero' })
+        items.forEach(item => this.header.children(item))
+        return this.header
+    }
 }
